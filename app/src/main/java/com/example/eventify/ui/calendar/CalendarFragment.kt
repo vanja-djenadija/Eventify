628748157e158fc029b776087c20a9a194efc294
vanja-djenadija <130909026+vanja-djenadija@users.noreply.github.com>
2023-06-05 18:15:16 +0200

    Navigation done Changed launcher icon

--- app/src/main/java/com/example/eventify/ui/calendar/CalendarFragment.kt
+++ app/src/main/java/com/example/eventify/ui/calendar/CalendarFragment.kt
@@ -1,4 +1,4 @@
-package com.example.eventify.ui.notifications
+package com.example.eventify.ui.calendar
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -7,11 +7,11 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.example.eventify.databinding.FragmentNotificationsBinding
+import com.example.eventify.databinding.FragmentCalendarBinding
 
-class NotificationsFragment : Fragment() {
+class CalendarFragment : Fragment() {
 
-    private var _binding: FragmentNotificationsBinding? = null
+    private var _binding: FragmentCalendarBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -23,12 +23,12 @@ class NotificationsFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         val notificationsViewModel =
-            ViewModelProvider(this).get(NotificationsViewModel::class.java)
+            ViewModelProvider(this)[CalendarViewModel::class.java]
 
-        _binding = FragmentNotificationsBinding.inflate(inflater, container, false)
+        _binding = FragmentCalendarBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textNotifications
+        val textView: TextView = binding.textCalendar
         notificationsViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
         }
---
+++ app/src/main/java/com/example/eventify/ui/settings/SettingsFragment.kt
@@ -0,0 +1,12 @@
+package com.example.eventify.ui.settings
+
+import android.os.Bundle
+import androidx.preference.PreferenceFragmentCompat
+import com.example.eventify.R
+
+class SettingsFragment : PreferenceFragmentCompat() {
+
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+        setPreferencesFromResource(R.xml.root_preferences, rootKey)
+    }
+}
